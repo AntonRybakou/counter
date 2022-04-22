bdb92f4082012f05cdcd0a8633b2c2b6914fb78d
ToshaBY <anton.rybakou@gmail.com>
2022-04-22 22:28:13 +0300

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Counter} from './components/Counter/Counter'
+import {Button} from "./components/Button/Button";
 
 function App() {
-    const [state, setState] = useState(0)
-    const [disable, setDisable] = useState(false)
+    const startCount = 0;
+    const endCount = 5
+    const [state, setState] = useState(startCount)
     const increment = () => {
-        setState( (a) => ++a );
+        setState( a => ++a );
+    }
+    const decrement = () => {
+        setState( a => --a );
     }
     const reset = () => {
         setState(0);
-        setDisable(false)
     }
-    useEffect(() => {
-        if (state === 5) {
-            setDisable(true)
-        }
-    }, [state]);
 
     return (
         <div className="App">
-            <Counter data={state} increment={increment} reset={reset} disable={disable}/>
+            <Counter
+                data={state}
+                endCount={endCount}/>
+            <div className='buttons'>
+                <Button
+                    name={'+'}
+                    callBack={increment}
+                    disable={state === endCount}/>
+                <Button
+                    name={'-'}
+                    callBack={decrement}
+                    disable={state === endCount}/>
+                <Button
+                    name={'Reset'}
+                    callBack={reset}/>
+            </div>
         </div>
     );
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import {Button} from "../Button/Button";
 import style from './Counter.module.css'
 
 type CounterPropsType = {
     data: number,
-    increment: () => void,
-    reset: () => void,
-    disable: boolean,
+    endCount: number,
 }
 
-export const Counter: React.FC<CounterPropsType> = ({data, increment, reset, disable}) => {
-    return (
-        <div className={style.counter}>
-            <div className={ (data === 5) ? style.red : style.count}>{data}</div>
-            <div className={style.buttons}>
-                <Button
-                    name={'+'}
-                    callBack={increment}
-                    disable={disable}/>
-                <Button
-                    name={'Reset'}
-                    callBack={reset}/>
-            </div>
-        </div>
-    );
+export const Counter: React.FC<CounterPropsType> = ({data, endCount}) => {
+    return <div className={(data === endCount) ? style.red : style.count}>{data}</div>
 };
